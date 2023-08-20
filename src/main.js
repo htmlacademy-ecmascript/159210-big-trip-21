d85ea652ac9edb1b1d71c363856d6d0ab7c3f142
slipfly <slipfly@gmail.com>
2023-08-20 19:43:16 +0300

    компонент списка отрисовывается на странице

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { FilterView } from './view/filter-view.js';
 import { SortView } from './view/sort-view.js';
+import { ListView } from './view/list-view.js';
 import { render } from './render.js';
 
 const filtersContainer = document.querySelector('.trip-controls__filters');
@@ -7,3 +8,4 @@ const tripEvents = document.querySelector('.trip-events');
 
 render(new FilterView(), filtersContainer);
 render(new SortView(), tripEvents);
+render(new ListView(), tripEvents);

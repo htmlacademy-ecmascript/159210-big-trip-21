1679a39840e1af477870b7dc3dd577a6eccb216a
slipfly <slipfly@gmail.com>
2023-08-20 15:52:16 +0300

    компонент сортировки добавляется на страницу

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import { FilterView } from './view/filter-view.js';
+import { SortView } from './view/sort-view.js';
 import { render } from './render.js';
 
 const filtersContainer = document.querySelector('.trip-controls__filters');
+const tripEvents = document.querySelector('.trip-events');
 
 render(new FilterView(), filtersContainer);
+render(new SortView(), tripEvents);

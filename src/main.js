690cd609ba88940cbef463aa6c88728a0caa98c4
slipfly <slipfly@gmail.com>
2023-08-20 20:02:03 +0300

    рендер формы редактирования точки маршрута

--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@ import { FilterView } from './view/filter-view.js';
 import { SortView } from './view/sort-view.js';
 import { ListView } from './view/list-view.js';
 import { ListItemView } from './view/list-item-view.js';
-import { render } from './render.js';
+import { PointEditView } from './view/point-edit-view.js';
+import { render, RenderPosition } from './render.js';
 
 const ENTRY_COUNT = 3;
+const CHANGING_ENTRY = 0;
 
 const filtersContainer = document.querySelector('.trip-controls__filters');
 const tripEvents = document.querySelector('.trip-events');
@@ -18,3 +20,6 @@ const eventsList = tripEvents.querySelector('.trip-events__list');
 for (let i = 0; i < ENTRY_COUNT; i++) {
   render(new ListItemView(), eventsList);
 }
+const changingEventItem = eventsList.querySelectorAll('.trip-events__item')[CHANGING_ENTRY];
+
+render(new PointEditView(), changingEventItem, RenderPosition.AFTERBEGIN);

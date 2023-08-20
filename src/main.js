7d7edcefb736bbfebbf98510b034ea0cd8580d70
slipfly <slipfly@gmail.com>
2023-08-20 19:51:31 +0300

    три элемента списка отрисовываются на странице

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import { FilterView } from './view/filter-view.js';
 import { SortView } from './view/sort-view.js';
 import { ListView } from './view/list-view.js';
+import { ListItemView } from './view/list-item-view.js';
 import { render } from './render.js';
+
+const ENTRY_COUNT = 3;
 
 const filtersContainer = document.querySelector('.trip-controls__filters');
 const tripEvents = document.querySelector('.trip-events');
@@ -9,3 +12,9 @@ const tripEvents = document.querySelector('.trip-events');
 render(new FilterView(), filtersContainer);
 render(new SortView(), tripEvents);
 render(new ListView(), tripEvents);
+
+const eventsList = tripEvents.querySelector('.trip-events__list');
+
+for (let i = 0; i < ENTRY_COUNT; i++) {
+  render(new ListItemView(), eventsList);
+}

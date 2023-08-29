7bac0b54064075bdf97e926538b838341567cd5d
slipfly <slipfly@gmail.com>
2023-08-29 22:20:39 +0300

    шаблонизирован рендер евентов

--- src/presenter/page-presenter.js
+++ src/presenter/page-presenter.js
@@ -1,7 +1,5 @@
-import { ENTRY_COUNT } from '../const.js';
 import { SortView } from '../view/sort-view.js';
 import { ListView } from '../view/list-view.js';
-import { EventLineView } from '../view/event-line-view.js';
 import { EventEditView } from '../view/event-edit-view.js';
 import { ListItemView } from '../view/list-item-view.js';
 import { EventHeaderView } from '../view/event-header-view.js';
@@ -9,13 +7,17 @@ import { EventDetailsView } from '../view/event-details-view.js';
 import { EventOfferView } from '../view/event-offer-view.js';
 import { EventDestinationView } from '../view/event-destination-view.js';
 import { render } from '../render.js';
+import { sortByDate } from '../utils.js';
+import { EmptyListView } from '../view/empty-list-view.js';
+import { getArrayOfEvents } from '../mock/event.js';
+
+let eventsArray = getArrayOfEvents();
 
 class PagePresenter {
-  sortComponent = new SortView();
-  listComponent = new ListView();
-
   constructor ({container}) {
     this.container = container;
+    this.sortComponent = new SortView();
+    this.listComponent = new ListView();
   }
 
   init() {
@@ -23,20 +25,26 @@ class PagePresenter {
     const editingForm = new EventEditView();
     const editingDetails = new EventDetailsView();
 
-    render(this.sortComponent, this.container);
-    render(this.listComponent, this.container);
+    if (eventsArray.length > 0) {
+      eventsArray = sortByDate(eventsArray);
+      render(this.sortComponent, this.container);
+      render(this.listComponent, this.container);
 
-    render(editingItem, this.listComponent.getElement());
-    render(editingForm, editingItem.getElement());
-    render(new EventHeaderView, editingForm.getElement());
-    render(editingDetails, editingForm.getElement());
-    render(new EventOfferView, editingDetails.getElement());
-    render(new EventDestinationView, editingDetails.getElement());
+      render(editingItem, this.listComponent.getElement());
+      render(editingForm, editingItem.getElement());
 
-    for (let i = 0; i < ENTRY_COUNT; i++) {
-      const newItem = new ListItemView();
-      render(newItem, this.listComponent.getElement());
-      render(new EventLineView, newItem.getElement());
+      render(new EventHeaderView(eventsArray[0]), editingForm.getElement());
+      render(editingDetails, editingForm.getElement());
+      render(new EventOfferView(eventsArray[0]), editingDetails.getElement());
+      render(new EventDestinationView(eventsArray[0]), editingDetails.getElement());
+
+      for (let i = 1; i < eventsArray.length; i++) {
+        const newItem = new ListItemView();
+        render(newItem, this.listComponent.getElement());
+        render(eventsArray[i], newItem.getElement());
+      }
+    } else {
+      render(new EmptyListView, this.container);
     }
   }
 }

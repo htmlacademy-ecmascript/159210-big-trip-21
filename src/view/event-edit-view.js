b5e92aba1cb5a317f6d3e078f91206a80b7a56f9
slipfly <slipfly@gmail.com>
2023-09-26 00:08:33 +0300

    код совмещён, добавлены обработчики состояний

--- src/view/event-edit-view.js
+++ src/view/event-edit-view.js
@@ -1,58 +1,133 @@
-import { render } from '../framework/render.js';
-import AbstractView from '../framework/view/abstract-view.js';
-import EventDestinationView from './event-destination-view.js';
-import EventDetailsView from './event-details-view.js';
+import AbstractStatefulView from '../framework/view/abstract-stateful-view.js';
+import { OFFERS, OFFERS_PRICES, DESTINATIONS,
+  PHOTOS_BORDER_NUMS, PHOTOS_COUNT, PHOTOS_SRC } from '../const.js';
+import { getRandomInteger } from '../utils/common.js';
 import EventHeaderView from './event-header-view.js';
-import EventOfferView from './event-offer-view.js';
+import { RenderPosition, render } from '../framework/render.js';
 
-function createPointEditTemplate() {
+function createOffersList({ offers }) {
+  let offersList = '';
+  for (const [key, value] of Object.entries(OFFERS)) {
+    offersList += `<div class="event__offer-selector">
+          <input class="event__offer-checkbox  visually-hidden" id="event-offer-${key}-1" type="checkbox" name="event-offer-${key}" ${offers.includes(String(key)) ? 'checked' : ''}>
+          <label class="event__offer-label" for="event-offer-${key}-1">
+            <span class="event__offer-title">${value}</span>
+            &plus;&euro;&nbsp;
+            <span class="event__offer-price">${OFFERS_PRICES[key]}</span>
+          </label>
+        </div>`;
+  }
+  return offersList;
+}
+
+function createOfferTemplate(event, isOffers) {
+  return (
+    isOffers ? `<section class="event__section  event__section--offers">
+      <h3 class="event__section-title  event__section-title--offers">Offers</h3>
+
+      <div class="event__available-offers">
+        ${createOffersList(event)}
+      </div>
+    </section>` : ''
+  );
+}
+
+function createPhotos() {
+  const count = getRandomInteger(PHOTOS_COUNT.min, PHOTOS_COUNT.max);
+  let photos = '';
+  for (let i = 0; i < count; i++) {
+    photos += `<img class="event__photo" src="${PHOTOS_SRC}${getRandomInteger(PHOTOS_BORDER_NUMS.min, PHOTOS_BORDER_NUMS.max)}" alt="Event photo">\n`;
+  }
+
+  return photos;
+}
+
+function createDestinationTemplate({ destination, isDestination }) {
+  return (
+    isDestination ? `<section class="event__section  event__section--destination">
+      <h3 class="event__section-title  event__section-title--destination">Destination</h3>
+      <p class="event__destination-description">${DESTINATIONS[destination]}</p>
+
+      <div class="event__photos-container">
+        <div class="event__photos-tape">
+          ${createPhotos()}
+        </div>
+      </div>
+    </section>` : ''
+  );
+}
+
+function createEventEditTemplate(event, isOffers, isDestination) {
   return (
     `<form class="event event--edit" action="#" method="post">
+      <section class="event__details">
+        ${createOfferTemplate(event, isOffers)}
+        ${createDestinationTemplate(event, isDestination)}
+      </section>
     </form>`
   );
 }
 
-export default class EventEditView extends AbstractView {
+export default class EventEditView extends AbstractStatefulView {
   #header = null;
-  #offer = null;
-  #destination = null;
   #onSubmitClick = null;
   #onCancelClick = null;
-  #event = null;
-
-  #details = new EventDetailsView();
 
   constructor({ event, onSubmitClick, onCancelClick, onRollupClick }) {
     super();
+    this._setState(EventEditView.parseEventToState(event));
     this.#header = new EventHeaderView({ event, onRollupClick });
-    this.#offer = new EventOfferView(event);
-    this.#destination = new EventDestinationView(event);
     this.#onSubmitClick = onSubmitClick;
     this.#onCancelClick = onCancelClick;
-    this.#event = event;
 
     this.element.addEventListener('submit', this.#submitClickHandler);
     this.element.addEventListener('reset', this.#cancelClickHandler);
   }
 
   get template() {
-    return createPointEditTemplate();
+    return createEventEditTemplate(
+      this._state,
+      this._state.isOffers,
+      this._state.isDestination
+    );
   }
 
   init() {
-    render(this.#header, this.element);
-    render(this.#details, this.element);
-    render(this.#offer, this.#details.element);
-    render(this.#destination, this.#details.element);
+    render(this.#header, this.element, RenderPosition.AFTERBEGIN);
   }
 
   #submitClickHandler = (evt) => {
     evt.preventDefault();
-    this.#onSubmitClick(this.#event);
+    this.#onSubmitClick(EventEditView.parseEventToState(this._state));
   };
 
   #cancelClickHandler = (evt) => {
     evt.preventDefault();
     this.#onCancelClick();
   };
+
+  static parseEventToState(event) {
+    return {
+      ...event,
+      isDestination: event.destination !== null,
+      isOffers: event.offers.length > 0
+    };
+  }
+
+  static parseStateToEvent(state) {
+    const event = { ...state };
+
+    if (!event.isDestination) {
+      event.destination = null;
+    }
+
+    if (!event.isOffers) {
+      event.offers = [];
+    }
+
+    delete event.isDestination;
+    delete event.isOffers;
+
+    return event;
+  }
 }
